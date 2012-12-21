84dba805d4135a042def16446a591dd12446f843
simon <ficstudio@yahoo.com.tw>
2012-12-21 13:41:22 +0800

    v0.1.6

--- darkchess.py
+++ darkchess.py
@@ -563,6 +563,45 @@ def move_max_value(orgx, orgy, destx, desty, my_chess, a_map, org_value, owner_c
         move_max_value(orgx, orgy, destx, desty, my_chess, a_map, org_value, owner_color, i, j+1)
         move_max_value(orgx, orgy, destx, desty, my_chess, a_map, org_value, owner_color, i, j-1)    
 
+def caca(org, dest, my_chess, a_map, owner_color):        
+    if org == None:
+        return 0
+    
+    (orgy, orgx) = org
+    (desty, destx) = dest
+    
+    m = a_map[orgy][orgx]
+    if m == None:
+        return 0
+    elif 2 == my_chess[m[0]][m[1]].value:
+        return 0
+    
+    if desty-1 >= 0 and destx-1 >=0:
+        n = a_map[desty-1][destx-1]
+        if n == None:
+            return 0
+        elif my_chess[n[0]][n[1]].color != my_chess[m[0]][m[1]].color and my_chess[n[0]][n[1]].value <= my_chess[m[0]][m[1]].value:
+            return 1
+    if desty-1 >= 0 and destx+1 <=7:
+        n = a_map[desty-1][destx+1]
+        if n == None:
+            return 0
+        elif my_chess[n[0]][n[1]].color != my_chess[m[0]][m[1]].color and my_chess[n[0]][n[1]].value <= my_chess[m[0]][m[1]].value:
+            return 1
+    if desty+1 <= 3 and destx-1 >= 0:
+        n = a_map[desty+1][destx-1]
+        if n == None:
+            return 0
+        elif my_chess[n[0]][n[1]].color != my_chess[m[0]][m[1]].color and my_chess[n[0]][n[1]].value <= my_chess[m[0]][m[1]].value:
+            return 1
+    if desty+1 <= 3 and destx+1 <= 7:
+        n = a_map[desty+1][destx+1]
+        if n == None:
+            return 0
+        elif my_chess[n[0]][n[1]].color != my_chess[m[0]][m[1]].color and my_chess[n[0]][n[1]].value <= my_chess[m[0]][m[1]].value:
+            return 1        
+        
+        
 def near2_have_same_value(org, my_chess, a_map, owner_color):
     if org == None:
         return 0
@@ -643,6 +682,8 @@ def move_score(org, dest, my_chess, a_map, owner_color):
         org_value = my_chess[a_map[orgy][orgx][0]][a_map[orgy][orgx][1]].value
         if 1 == near2_have_same_value(org, my_chess, a_map, owner_color):
             return -1
+        elif 1 == caca(org, dest, my_chess, a_map, owner_color):
+            return org_value+1
         move_max_value(orgx, orgy, destx, desty, my_chess, a_map, org_value, my_chess[a_map[orgy][orgx][0]][a_map[orgy][orgx][1]].color, desty, destx)
         if 8 == max_value:
             return org_value/2
